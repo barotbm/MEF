9dacb1895fc67735f1cbfe30f72ba9dfcc09f834
Bhavik Barot <bhavik.barot@nagarro.com>
2018-10-20 07:57:28 -0700

    Added the .NET Core project.

---
+++ MEF_Console/MEF_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MEF_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
